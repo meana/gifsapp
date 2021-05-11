58073cbc9b63947455d625e58d6136bc85c18962
meana <ramon.meana@gmail.com>
2021-05-11 20:11:16 +0200

    http request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,11 +6,14 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   
+  private apiKey: string = 'NgORlAKP9A0a4ylcS03j3hHmoA1v2Z2I';
   private _historial: string[] = []
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient ) {}
 
   buscarGifs(query: string){
     
@@ -19,5 +23,12 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
     }
+
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=NgORlAKP9A0a4ylcS03j3hHmoA1v2Z2I&q=dbz&limit=10')
+      .subscribe((response: any) => {
+        console.log(response.data);
+      })
+
+
   }
 }
